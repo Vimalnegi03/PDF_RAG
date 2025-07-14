5ea95d0237917448fa4b0acd2a43ebd21bbb51d4
Vimal <vm.21cs48@thdcihet.ac.in>
2025-07-14 17:52:10 +0000

    added mongodb save

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile
-from uuid import uuid4
 from .utils.file import save_to_disk
+from .db.collections.files import files_collection, FileSchema
 app = FastAPI()
 
 
@@ -11,8 +11,24 @@ def hello():
 
 @app.post("/upload")
 async def upload_file(file: UploadFile):
-    id = uuid4()
-    file_path = f"/mnt/uploads/{id}/{file.filename}"
+  
+    db_file = await files_collection.insert_one(
+        document=FileSchema(
+            name=file.filename,
+            status="saving"
+        )
+    )
+    file_path = f"/mnt/uploads/{str(db_file.inserted_id)}/{file.filename}"
     # saved file in disk
     await save_to_disk(file=await file.read(), path=file_path)
-    return {"file_id": id} 
+    
+    # push to queue
+    print("File ko queue mai push kr do")
+    # MOngo Save
+    await files_collection.update_one({"_id": db_file.inserted_id}, {
+        "$set": {
+            "status": "queued"
+        }
+    })
+    
+    return {"file_id": str(db_file.inserted_id)}
