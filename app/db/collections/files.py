c7ec268774f692530112dc3d5e2dd626a42a2ec3
Vimal <vm.21cs48@thdcihet.ac.in>
2025-07-15 07:33:14 +0000

    added resume roaster

--- app/db/collections/files.py
+++ app/db/collections/files.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Optional
 from pydantic import Field
 from pymongo.asynchronous.collection import AsyncCollection
 from ..db import database
@@ -7,6 +7,7 @@ from ..db import database
 class FileSchema(TypedDict):
     name: str = Field(..., description="Name of the file")
     status: str = Field(..., description="Status of the file")
+    result: Optional[str] = Field(None, description="the result from AI")
     
     
 COLLECTION_NAME = "files"
--- app/server.py
+++ app/server.py
@@ -1,14 +1,26 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Path
 from .utils.file import save_to_disk
 from .db.collections.files import files_collection, FileSchema
 from .queue.q import q
-from .queue.workers import process_file 
+from .queue.workers import process_file
+from bson import ObjectId
 app = FastAPI()
 
 
 @app.get("/")
 def hello():
     return {"status": "healthy"}
+
+
+@app.get("/{id}")
+async def get_file_by_id(id: str = Path(..., description="Id of the file")):
+    db_file = await files_collection.find_one({"_id": ObjectId(id)})
+    return {
+        "_id": str(db_file["_id"]),
+        "name": db_file["name"],
+        "status": db_file["status"],
+        "result": db_file["result"] if "result" in db_file else None,
+    }
 
 
 @app.post("/upload")
@@ -25,7 +37,8 @@ async def upload_file(file: UploadFile):
     await save_to_disk(file=await file.read(), path=file_path)
     
     # push to queue
-    q.enqueue(process_file, str(db_file.inserted_id))
+    q.enqueue(process_file, str(db_file.inserted_id), file_path)
+
     # MOngo Save
     await files_collection.update_one({"_id": db_file.inserted_id}, {
         "$set": {
